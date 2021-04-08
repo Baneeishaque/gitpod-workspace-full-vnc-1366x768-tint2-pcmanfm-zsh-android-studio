c339adbe10a5e729ec4666330511a3f0ecd7d4cf
Banee Ishaque K <baneeishaque@hotmail.com>
2021-04-08 18:40:19 +0000

    Thu 08 Apr 2021 06:40:19 PM UTC

--- pushLatest.bash
+++ pushLatest.bash
@@ -1,4 +1,4 @@
-git restore AndroidStudio4.1-config/options/window.state.xml
+# git restore AndroidStudio4.1-config/options/window.state.xml AndroidStudio4.1-config/options/actionSummary.xml AndroidStudio4.1-config/options/other.xml AndroidStudio4.1-config/options/recentProjects.xml AndroidStudio4.1-config/options/usage.statistics.xml AndroidStudio4.1-local/availables.xml AndroidStudio4.1-local/availables.xml.etag AndroidStudio4.1-local/extensions.xml
 git add .
 git commit -m "`date`"
 git push
